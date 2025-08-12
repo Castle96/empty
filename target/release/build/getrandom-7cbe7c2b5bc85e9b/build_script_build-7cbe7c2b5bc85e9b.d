09217c8cbedfc7444213c30aa223ce6d1acc69ec
Castle96 <kcastle96@gmail.com>
2025-08-12 14:51:14 -0500

    microkernel written in rust

---
+++ target/release/build/getrandom-7cbe7c2b5bc85e9b/build_script_build-7cbe7c2b5bc85e9b.d
@@ -0,0 +1,5 @@
+/home/kyle/Documents/empty/target/release/build/getrandom-7cbe7c2b5bc85e9b/build_script_build-7cbe7c2b5bc85e9b.d: /home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs
+
+/home/kyle/Documents/empty/target/release/build/getrandom-7cbe7c2b5bc85e9b/build_script_build-7cbe7c2b5bc85e9b: /home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs
+
+/home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs:
---
+++ target/release/build/proc-macro2-bebc665534c12f0e/build_script_build-bebc665534c12f0e.d
@@ -0,0 +1,5 @@
+/home/kyle/Documents/empty/target/release/build/proc-macro2-bebc665534c12f0e/build_script_build-bebc665534c12f0e.d: /home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
+
+/home/kyle/Documents/empty/target/release/build/proc-macro2-bebc665534c12f0e/build_script_build-bebc665534c12f0e: /home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
+
+/home/kyle/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
